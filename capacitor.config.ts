c6982a87e1ca762645a3d6c3c54993f67c82ee52
Crakchi <crakchi@gmail.com>
2024-01-12 13:06:55 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ComponentesReutilizables_Insumo2_Rivera',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
